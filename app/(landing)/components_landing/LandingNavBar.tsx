0c96beb3967b7486020fd4ae57a78346e93b75b9
Jay.develops <jaymations1234@gmail.com>
2024-01-23 20:14:12 -0800

    optimized imports on LandingNavBar.tsx

--- app/(landing)/components_landing/LandingNavBar.tsx
+++ app/(landing)/components_landing/LandingNavBar.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
 import UserAuthenticated from "@/app/(landing)/components_landing/UserAuthenticated";
-import {ModeToggle} from "@/components/ui/mode-toggle";
 
 export default function LandingNavBar() {
 
