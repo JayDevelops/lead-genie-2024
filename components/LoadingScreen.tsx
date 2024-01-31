aa71152a49b8759667a61e2a74b6dc7b890609bd
Jay.develops <jaymations1234@gmail.com>
2024-01-30 19:35:18 -0800

    Fixed loading screen bg color

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -3,7 +3,7 @@ import {RingLoader} from "react-spinners";
 
 export default function LoadingScreen() {
     return (
-        <div className="z-50 flex h-screen w-screen items-center justify-center bg-primary">
+        <div className="z-50 flex h-screen w-screen items-center justify-center bg-secondary">
             <div className="relative flex flex-col items-center justify-center">
                 <div className="z-10 text-5xl font-bold text-primary">
                     LeadGenie
